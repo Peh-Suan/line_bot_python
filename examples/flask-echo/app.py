30696540e02786c402deb4c888d98bd1bea7d83b
黃柏瑄 <benson32169@gmail.com>
2023-01-12 16:35:23 +0800

    Update app.py

--- examples/flask-echo/app.py
+++ examples/flask-echo/app.py
@@ -89,7 +89,7 @@ def callback():
 
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text=event.message.text+'_test')
+            TextSendMessage(text=to_reply)
         )
         
 
